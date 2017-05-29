9b546982eeb1138502a4d5d9e7e82dc05f249c89
Alex Carlin <carlin@ucdavis.edu>
2017-05-29 02:49:00 -0700

    Code cleanup

--- Wheels/ViewController.swift
+++ Wheels/ViewController.swift
@@ -9,15 +9,36 @@
 import UIKit
 import AVFoundation
 
+class tape_recorder {
+    func load(tape: String) {
+        print("Loading", tape)
+    }
+}
+
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // create a tape recorder
+    let tp = tape_recorder()
+
     // array of recordings
-    var animals = [String]()
-    var timer: Timer!
+    var tapes = [String]()
 
+    // this is used during recording
+    var timer: Timer !
 
-    func updateAudioMeter(_ timer:Timer) {
+    // timestamp on recording (and meters)
+    @IBOutlet var timestamp: UILabel !
 
+    var audioRecorder: AVAudioRecorder !
+
+    let recordSettings = [
+        AVSampleRateKey: NSNumber(value: Float(44100.0)),
+        AVFormatIDKey: NSNumber(value:Int32(kAudioFormatMPEG4AAC)),
+        AVNumberOfChannelsKey: NSNumber(value: Int32(1)),
+        AVEncoderAudioQualityKey: NSNumber(value: Int32(AVAudioQuality.medium.rawValue))
+    ]
+
+    func updateAudioMeter(_ timer: Timer) {
         if audioRecorder.isRecording {
             let hour = Int(audioRecorder.currentTime / 360)
             let min = Int(audioRecorder.currentTime / 60)
@@ -25,14 +46,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             let s = String(format: "%02d:%02d:%02d", hour, min, sec)
             timestamp.text = s
             audioRecorder.updateMeters()
-            // if you want to draw some graphics...
-            var apc0 = audioRecorder.averagePower(forChannel:0)
-            print(apc0)
-            var peak0 = audioRecorder.peakPower(forChannel:0)
-            print(peak0)
+            let avg = audioRecorder.averagePower(forChannel:0)
+            print(avg)
+            let peak = audioRecorder.peakPower(forChannel:0)
+            print(Int(peak))
+            level0.text = String(format:"%f", avg)
+            level1.text = String(format:"%f", peak)
+            level_bar.setProgress(1-(peak * -(1/160)), animated:false)
+            average_bar.setProgress(1-(avg * -(1/120)), animated:false)
         }
     }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -60,7 +83,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
             let my_files = directoryContents.flatMap({$0.lastPathComponent})
             for fn in my_files {
-                animals.append(fn)
+                tapes.append(fn)
             }
 
 
@@ -79,15 +102,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         } catch {}
     }
 
-    var audioRecorder:AVAudioRecorder!
-
-    let recordSettings = [
-        AVSampleRateKey : NSNumber(value: Float(44100.0)),
-        AVFormatIDKey : NSNumber(value:Int32(kAudioFormatMPEG4AAC)),
-        AVNumberOfChannelsKey : NSNumber(value: Int32(1)),
-        AVEncoderAudioQualityKey :
-            NSNumber(value: Int32(AVAudioQuality.medium.rawValue))]
-
 
     func directoryURL() -> URL? {
         let fileManager = FileManager.default
@@ -103,8 +117,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return soundURL
     }
 
-    @IBOutlet var timestamp: UILabel!
 
+    // record button and action
     @IBOutlet weak var recordButton: UIButton!
 
     @IBAction func doRecordAction(_ sender: AnyObject) {
@@ -114,27 +128,32 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             do {
                 try audioSession.setActive(true)
                 audioRecorder.record()
-                self.timer = Timer.scheduledTimer(timeInterval: 0.1,
-                                                       target:self,
-                                                       selector:#selector(updateAudioMeter(_:)),
-                                                       userInfo:nil,
-                                                       repeats:true)
-            } catch {}
+                self.timer = Timer.scheduledTimer(timeInterval: 0.1,target:self,selector:#selector(updateAudioMeter(_:)),userInfo:nil,repeats:true)
+            } catch {
+              // what to do here?
+            }
         }
     }
 
+    // stop button
     @IBOutlet weak var stopButton: UIButton!
 
     @IBAction func doStopAction(_ sender:
         AnyObject) {
         print("Stop recording")
-        audioRecorder.stop()
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setActive(false)
-        } catch {}
+        if audioRecorder.isRecording {
+          audioRecorder.stop()
+          // save the tape!
+          let audioSession = AVAudioSession.sharedInstance()
+          do {
+              try audioSession.setActive(false)
+          } catch {}
+          // no idea what the above 4 lines do
+        }
     }
 
+    // table view
+    
     // cell reuse id (cells that scroll out of view can be reused)
     let cellReuseIdentifier = "cell"
 
@@ -143,7 +162,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.animals.count
+        return self.tapes.count
     }
 
     // create a cell for each table view row
@@ -153,29 +172,47 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
 
         // set the text from the data model
-        cell.textLabel?.text = self.animals[indexPath.row]
+        cell.textLabel?.text = self.tapes[indexPath.row]
         return cell
     }
 
     // method to run when table view cell is tapped
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("You tapped cell number \(indexPath.row).")
+        // get the tape in this cell
+        // self.tapes[indexPath.row]
+        print(indexPath.row)
+        print(self.tapes)
+        print(self.tapes[indexPath.row])
+        let tapped_tape = self.tapes[indexPath.row]
+        print( tapped_tape )
+        // load this tape
+        tp.load(tape:tapped_tape)
     }
 
     // this method handles row deletion
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-
         if editingStyle == .delete {
-
             // remove the item from the data model
-            animals.remove(at: indexPath.row)
-
+            tapes.remove(at: indexPath.row)
             // delete the table view row
             tableView.deleteRows(at: [indexPath], with: .fade)
-
         } else if editingStyle == .insert {
             // Not used in our example, but if you were adding a new row, this is where you would do it.
         }
     }
+
+    // meters
+    @IBOutlet weak var level0: UILabel!
+    @IBOutlet weak var level1: UILabel!
+    @IBOutlet weak var level_bar: UIProgressView!
+    @IBOutlet weak var average_bar: UIProgressView!
+
+    // tape playback interface
+    @IBOutlet weak var play: UIButton!
+    @IBAction func doPlay(_ sender: UIButton) {
+        print("Play was pressed")
+    }
+
 
 }
